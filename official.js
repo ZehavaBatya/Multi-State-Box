101b0b8dbb1d4189396e617ff5efb8d6947c26f1
Annaliese B <84485729+ZehavaBatya@users.noreply.github.com>
2022-10-03 22:18:15 -0400

    Create official.js

---
+++ official.js
@@ -0,0 +1,47 @@
+$w.onReady(function () {
+
+    const blackColor = "#000000"; 
+    const whiteColor = "#FFFFFF"; 
+    
+
+    $w('#button1').onClick(() => {
+        $w('#statebox8').changeState("state1");
+    });
+    $w('#button2').onClick(() => {
+        $w('#statebox8').changeState("state2");
+    });
+    $w('#button3').onClick(() => {
+        $w('#statebox8').changeState("state3");
+    });
+    $w('#button4').onClick(() => {
+        $w('#statebox8').changeState("state4");
+    });
+	$w('#button5').onClick(() => {
+        $w('#statebox8').changeState("state5");
+    });
+    $w('#button6').onClick(() => {
+        $w('#statebox8').changeState("state6");
+    });
+    $w('#button7').onClick(() => {
+        $w('#statebox8').changeState("state7");
+    });
+    $w('#button8').onClick(() => {
+        $w('#statebox8').changeState("state8");
+    });
+	$w('#button9').onClick(() => {
+        $w('#statebox8').changeState("state9");
+    });
+    $w('#button10').onClick(() => {
+        $w('#statebox8').changeState("state10");
+    });
+    $w('#button11').onClick(() => {
+        $w('#statebox8').changeState("state11");
+    });
+    $w('#button12').onClick(() => {
+        $w('#statebox8').changeState("state12");
+    });
+});
+
+
+
+    
